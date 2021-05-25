e7fba6d7699b1e64dd895d62d94e83f241723f27
lamtiev <klamtev@gmail.com>
2021-05-25 15:45:23 +0300

    test01

--- app/src/main/java/com/test/myapplication/MainActivity.java
+++ app/src/main/java/com/test/myapplication/MainActivity.java
@@ -1,30 +1,24 @@
 package com.test.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         process();
-//test
+        // test
         Log.i("==>", "Hello World");
-
     }
 
-    private void process() {
-        System.out.println(getText());
+    private void process() { System.out.println(getText());
     }
 
     private String getText() {
         return "NEXT";
     }
-
 }
